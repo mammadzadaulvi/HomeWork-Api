26e68b07996580c21d3ee7a09f2b65308ba77dda
mammadzadaulvi <ulvifm@code.edu.az>
2022-12-14 18:09:40 +0400

    .

---
+++ api-intro-master/Presentation/Controllers/CategoryController.cs
@@ -0,0 +1,60 @@
+﻿using Business.DTOs.Category.Request;
+using Business.Services.Abstraction;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _categoryService.GetAllAsync());
+        }
+
+        
+  
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _categoryService.GetAsync(id));
+        }
+
+   
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CategoryCreateDTO model)
+        {
+            return Ok(await _categoryService.CreateAsync(model));
+        }
+
+
+
+        [HttpPost("update")]
+        public async Task<IActionResult> Update(CategoryUpdateDTO model)
+        {
+            return Ok(await _categoryService.UpdateAsync(model));
+        }
+
+       
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return Ok(await _categoryService.DeleteAsync(id));
+        }
+    }
+}
